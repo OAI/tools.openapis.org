401ee5beedb24157c304cc7e1ed5057b3e6fa044
Chris Wood <chris@arborcomputing.com>
2022-02-25 16:59:08 +0000

    feat: Implementation of merge process as described in issue #1

--- gulpfile.js/lib/processors/openapi-tools-processor.js
+++
@@ -1,8 +0,0 @@
-const http = require('axios');
-const YAML = require('js-yaml');
-
-module.exports = async (source, url) => {
-  const response = await http.get(url);
-
-  return YAML.load(response.data).map((tool) => ({ source, ...tool }));
-};
