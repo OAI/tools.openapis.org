b26cd4b0cb7665050cab25e3ae1595bcfb269d2f
Chris Wood <chris@arborcomputing.com>
2024-01-26 17:25:33 +0000

    fix: Added tests for invalid URLs and more test cases

--- test/unit/lib/data/processors/open-tooling-repository-issues.js
+++ test/unit/lib/data/processors/open-tooling-repository-issues.js
@@ -1,4 +1,5 @@
 const { GraphQLClient } = require('graphql-request');
+const { resolve } = require('path');
 
 const chai = require('chai');
 const chap = require('chai-as-promised');
@@ -9,10 +10,11 @@ chai.use(chap);
 
 const { expect } = chai;
 
-const fn = require('../../../../lib/data/processors/open-tooling-repository-issues');
+const fn = require('../../../../../lib/data/processors/open-tooling-repository-issues');
 
 describe(__filename, () => {
-  dotenv.config({ path: `${__dirname}/../../../data/.env` });
+  const envFile = resolve(`${__dirname}/../../../../data/.env`);
+  dotenv.config({ path: envFile });
 
   const sandbox = sinon.createSandbox();
   let graphQlClientStub;
@@ -43,7 +45,7 @@ describe(__filename, () => {
               author: { login: 'issueRaiser' },
               createdAt: '2023-03-30T09:37:02Z',
               updatedAt: '2023-03-30T09:50:49Z',
-              url: 'https://github.com/OAI/Tooling/issues/0',
+              url: 'https://github.com/OAI/Tooling/issues/1',
             },
             {
               body: '## Tool Properties\r\n'
@@ -57,8 +59,27 @@ describe(__filename, () => {
               author: { login: 'issueRaiser' },
               createdAt: '2023-03-30T09:37:02Z',
               updatedAt: '2023-03-30T09:50:49Z',
-              url: 'https://github.com/OAI/Tooling/issues/-1',
-            }],
+              url: 'https://github.com/OAI/Tooling/issues/2',
+            },
+            {
+              body: '> **Please use this template for tools that cannot be tagged on GitHub. If your tool is on GitHub use the `openapi3` and `openapi31` tags to allow your data to be collected automatically.**\r\n\r\n'
+              + '## Tool Properties\r\n\r\n'
+              + '> **Please replace all placeholders marked in bold in the bullets below with the requested information. Use plain text for your information.**\r\n\r\n'
+              + '- Display name: **Apidog**\r\n'
+              + '- Description: **Apidog is an integrated collaboration platform for API documentation, API debugging, API mocking, and API automated testing that combines Postman + Swagger + Mock + JMeter to tackle the data synchronization problem among different systems using a set of systems and a set of data. API debugging, API data mocking, and API automated testing can be directly used without redefining as long as the API documentation is well-defined.**\r\n'
+              + '- Homepage: https://www.apidog.com\r\n\r\n'
+              + '## OpenAPI Versions\r\n\r\n'
+              + '> **Please indicate the versions of OpenAPI supported by your tool by marking them true or false below.**\r\n\r\n'
+              + '- 3.1: true\r\n'
+              + '- 3.0: true\r\n'
+              + '- 2.0: true\r\n\r\n',
+              number: 3,
+              author: { login: 'issueRaiser' },
+              createdAt: '2023-03-30T09:37:02Z',
+              updatedAt: '2023-03-30T09:50:49Z',
+              url: 'https://github.com/OAI/Tooling/issues/3',
+            },
+          ],
         },
       },
     };
@@ -76,7 +97,7 @@ describe(__filename, () => {
           createdAt: '2023-03-30T09:37:02Z',
           issueNumber: 1,
           updatedAt: '2023-03-30T09:50:49Z',
-          url: 'https://github.com/OAI/Tooling/issues/0',
+          url: 'https://github.com/OAI/Tooling/issues/1',
           status: 'open',
         },
       },
@@ -93,7 +114,24 @@ describe(__filename, () => {
           createdAt: '2023-03-30T09:37:02Z',
           issueNumber: 2,
           updatedAt: '2023-03-30T09:50:49Z',
-          url: 'https://github.com/OAI/Tooling/issues/-1',
+          url: 'https://github.com/OAI/Tooling/issues/2',
+          status: 'open',
+        },
+      },
+      {
+        name: 'Apidog',
+        source: ['Tooling repository issues'],
+        source_description: 'Apidog is an integrated collaboration platform for API documentation, API debugging, API mocking, and API automated testing that combines Postman + Swagger + Mock + JMeter to tackle the data synchronization problem among different systems using a set of systems and a set of data. API debugging, API data mocking, and API automated testing can be directly used without redefining as long as the API documentation is well-defined.',
+        link: 'https://www.apidog.com',
+        v3_1: true,
+        v3: true,
+        v2: true,
+        sourceIssueMetadata: {
+          author: 'issueRaiser',
+          createdAt: '2023-03-30T09:37:02Z',
+          issueNumber: 3,
+          updatedAt: '2023-03-30T09:50:49Z',
+          url: 'https://github.com/OAI/Tooling/issues/3',
           status: 'open',
         },
       },
@@ -109,23 +147,58 @@ describe(__filename, () => {
       await expect(fn({ title: 'Tooling repository issues', masterDataFileName: 'src/_data/tools.yaml' }))
         .to.be.rejectedWith('GH_API_TOKEN, GH_API_USERNAME, TOOLING_REPOSITORY_OWNER or TOOLING_REPOSITORY_REPO_NAME not set in environment when executing open-tooling-repository-issues.js');
     });
+    it('Filters tool with invalid URL', async () => {
+      dotenv.config({ path: envFile });
+      graphQlClientStub.returns(Promise.resolve({
+        repository: {
+          issues: {
+            nodes: [
+              {
+                body: '> **Please use this template for tools that cannot be tagged on GitHub. If your tool is on GitHub use the `openapi3` and `openapi31` tags to allow your data to be collected automatically.**\r\n\r\n'
+                + '## Tool Properties\r\n\r\n'
+                + '> **Please replace all placeholders marked in bold in the bullets below with the requested information. Use plain text for your information.**\r\n\r\n'
+                + '- Display name: Boilerplate\r\n'
+                + '- Description: Boilerplate'
+                + '- Homepage: www.lacking-protocol.com\r\n\r\n'
+                + '## OpenAPI Versions\r\n\r\n'
+                + '> **Please indicate the versions of OpenAPI supported by your tool by marking them true or false below.**\r\n\r\n'
+                + '- 3.1: true\r\n'
+                + '- 3.0: true\r\n'
+                + '- 2.0: true\r\n\r\n',
+                number: 3,
+                author: { login: 'issueRaiser' },
+                createdAt: '2023-03-30T09:37:02Z',
+                updatedAt: '2023-03-30T09:50:49Z',
+                url: 'https://github.com/OAI/Tooling/issues/3',
+              },
+            ],
+          },
+        },
+      }));
+
+      await expect(fn({
+        title: 'Tooling repository issues',
+        masterDataFileName: `${__dirname}/../../../../data/lib/data/processors/tools-no-existing-issues.yaml`,
+      }))
+        .to.eventually.deep.equal([]);
+    });
     it('Returns the expected number of tooling issues when querying GitHub GraphQL endpoint', async () => {
-      dotenv.config({ path: `${__dirname}/../../../data/.env` });
+      dotenv.config({ path: envFile });
       graphQlClientStub.returns(Promise.resolve(expectedGraphQlResponseBody));
 
       await expect(fn({
         title: 'Tooling repository issues',
-        masterDataFileName: `${__dirname}/../../../data/lib/data/processors/tools-no-existing-issues.yaml`,
+        masterDataFileName: `${__dirname}/../../../../data/lib/data/processors/tools-no-existing-issues.yaml`,
       }))
         .to.eventually.deep.equal(expectedTools);
     });
     it('Returns only new tooling issues when an issue already exists in the dataset (i.e. closing the issue failed)', async () => {
-      dotenv.config({ path: `${__dirname}/../../../data/.env` });
+      dotenv.config({ path: envFile });
       graphQlClientStub.returns(Promise.resolve(expectedGraphQlResponseBody));
 
       await expect(fn({
         title: 'Tooling repository issues',
-        masterDataFileName: `${__dirname}/../../../data/lib/data/processors/tools-existing-issues.yaml`,
+        masterDataFileName: `${__dirname}/../../../../data/lib/data/processors/tools-existing-issues.yaml`,
       }))
         .to.eventually.deep.equal([expectedTools[1]]);
     });
